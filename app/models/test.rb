bc00c54a5fdbcf5d35c597c1f026b0306b7d183c
Ekaterina Egorova <admin.karima@gmail.com>
2020-04-05 18:08:38 +0300

    assosiations for models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_many :tests_users
+  has_many :users, through: :tests_users
+  belongs_to :author, class_name: 'User', optional: true
 
   def self.from_category(category_name)
     Test.joins('JOIN categories ON categories.id = tests.category_id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :tests_users
+  has_many :tests, through: :tests_users
+  has_many :author_tests, foreign_key: :author_id, class_name: 'Test'
 
   def tests_by_level(level)
     Test.joins('JOIN tests_users ON tests_users.test_id = tests.id')
