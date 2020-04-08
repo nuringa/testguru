c386ba09ac818ab5359e1b072c0d88e73ddde02d
Ekaterina Egorova <admin.karima@gmail.com>
2020-04-08 11:23:11 +0300

    delete and rework reference migrations

--- db/migrate/20200407214407_remove_author_and_ref.rb
+++
@@ -1,6 +0,0 @@
-class RemoveAuthorAndRef < ActiveRecord::Migration[6.0]
-  def change
-    remove_foreign_key :tests, :users, column: :author_id
-    remove_column :tests, :author_id, :integer
-  end
-end
