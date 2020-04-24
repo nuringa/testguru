e4dca9fc97713583a4cbc71c126fa69f51724fd8
Ekaterina Egorova <admin.karima@gmail.com>
2020-04-24 15:03:54 +0300

    fix style after review

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -8,9 +8,7 @@ class TestPassage < ApplicationRecord
   before_validation :before_validation_set_current_question
 
   def accept!(answer_ids)
-    if correct_answer?(answer_ids)
-      self.correct_questions += 1
-    end
+    self.correct_questions += 1 if correct_answer?(answer_ids)
 
     save!
   end
