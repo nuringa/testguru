96f363dfb1b78d5eaaf285619cf7e19f72f0039f
Ekaterina Egorova <admin.karima@gmail.com>
2020-04-25 01:02:04 +0300

    add simple authentication for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
+
+  include Auth
+
   has_many :test_passages, dependent: :delete_all
   has_many :tests, through: :test_passages
   has_many :author_tests, foreign_key: :author_id, class_name: 'Test', dependent: :destroy
 
-  validates :name, presence: true
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test: test)
