fbc07846915a108186eea7a968d49462bb0c647e
Ekaterina Egorova <admin.karima@gmail.com>
2020-05-02 17:45:26 +0300

    fixes after review

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(user)
-    current_user.is_a?(Admin) ? admin_tests_path : (stored_location_for(user) || root_path)
+    user.admin? ? admin_tests_path : (stored_location_for(user) || root_path)
   end
 
   def configure_permitted_parameters
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
 
+  EMAIL = /\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
+  has_many :test_passages, dependent: :delete_all
+  has_many :tests, through: :test_passages
+  has_many :author_tests, foreign_key: :author_id, class_name: 'Test', dependent: :destroy
+
+  validates :first_name, presence: true
+
   devise :database_authenticatable,
          :registerable,
          :recoverable,
@@ -8,20 +16,15 @@ class User < ApplicationRecord
          :trackable,
          :confirmable
 
-  EMAIL = /\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
-
-  has_many :test_passages, dependent: :delete_all
-  has_many :tests, through: :test_passages
-  has_many :author_tests, foreign_key: :author_id, class_name: 'Test', dependent: :destroy
-
-  validates :email, presence: true, uniqueness: true, format: { with: EMAIL }
-  validates :first_name, presence: true
-
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test: test)
   end
 
   def tests_by_level(level)
     tests.where(level: level)
+  end
+
+  def admin?
+    is_a?(Admin)
   end
 end
