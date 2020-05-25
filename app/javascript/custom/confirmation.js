73f9d1dbf3878b34a7b401831fcf2af4a73feab5
Ekaterina Egorova <admin.karima@gmail.com>
2020-05-26 02:33:40 +0300

    password and confirmation matching

---
+++ app/javascript/custom/confirmation.js
@@ -0,0 +1,29 @@
+document.addEventListener('turbolinks:load', function() {
+  const control1 = document.getElementById('password-control')
+  const control2 = document.getElementById('confirmation-control')
+
+  if (control1 && control2) {
+    control1.addEventListener('input', showIcons)
+    control2.addEventListener('input', showIcons)
+  }
+})
+
+function showIcons() {
+  const password = document.getElementById('password-control')
+  const passwordConfirmation = document.getElementById('confirmation-control')
+
+  if (password.value === passwordConfirmation.value) {
+    document.querySelector('.octicon-check').classList.remove('hide')
+    document.querySelector('.octicon-circle-slash').classList.add('hide')
+    passwordConfirmation.classList.add('focus-success')
+  } else if (password.value === '' || passwordConfirmation.value === '') {
+    document.querySelector('.octicon-check').classList.add('hide')
+    document.querySelector('.octicon-circle-slash').classList.add('hide')
+    passwordConfirmation.classList.remove('focus-success', 'focus-warning')
+  } else {
+    document.querySelector('.octicon-check').classList.add('hide')
+    document.querySelector('.octicon-circle-slash').classList.remove('hide')
+    passwordConfirmation.classList.remove('focus-success')
+    passwordConfirmation.classList.add('focus-warning')
+  }
+}
