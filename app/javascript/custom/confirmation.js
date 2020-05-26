5f135d50b4cae1ee9dfb83c4a685774e4f46ca43
Ekaterina Egorova <admin.karima@gmail.com>
2020-05-26 14:13:08 +0300

    fix after review

--- app/javascript/custom/confirmation.js
+++ app/javascript/custom/confirmation.js
@@ -12,14 +12,14 @@ function showIcons() {
   const password = document.getElementById('password-control')
   const passwordConfirmation = document.getElementById('confirmation-control')
 
-  if (password.value === passwordConfirmation.value) {
-    document.querySelector('.octicon-check').classList.remove('hide')
-    document.querySelector('.octicon-circle-slash').classList.add('hide')
-    passwordConfirmation.classList.add('focus-success')
-  } else if (password.value === '' || passwordConfirmation.value === '') {
+  if (password.value === '' || passwordConfirmation.value === '') {
     document.querySelector('.octicon-check').classList.add('hide')
     document.querySelector('.octicon-circle-slash').classList.add('hide')
     passwordConfirmation.classList.remove('focus-success', 'focus-warning')
+  } else if (password.value === passwordConfirmation.value) {
+      document.querySelector('.octicon-check').classList.remove('hide')
+      document.querySelector('.octicon-circle-slash').classList.add('hide')
+      passwordConfirmation.classList.add('focus-success')
   } else {
     document.querySelector('.octicon-check').classList.add('hide')
     document.querySelector('.octicon-circle-slash').classList.remove('hide')
