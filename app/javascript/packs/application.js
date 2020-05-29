0cf18101cc6df7a64b28677f7f460ed48329d614
Ekaterina Egorova <admin.karima@gmail.com>
2020-05-29 13:59:36 +0300

    form_inline for editing tests js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 
 require("custom/sorting")
 require("custom/confirmation")
+require("custom/form_inline")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
