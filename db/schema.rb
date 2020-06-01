974efb3aaec05c0cf4dc3f14a3d0a54f4adb1bdf
Ekaterina Egorova <admin.karima@gmail.com>
2020-06-02 00:00:34 +0300

    compound index level title && seed fix for deploy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_142344) do
+ActiveRecord::Schema.define(version: 2020_06_01_194736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_142344) do
     t.bigint "author_id"
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,18 @@
 # uncomment if data erase is needed
-# Answer.delete_all
-# Question.delete_all
-# Test.delete_all
-# User.delete_all
-# TestsUser.delete_all
-# Category.delete_all
+Gist.delete_all
+TestPassage.delete_all
+Answer.delete_all
+Question.delete_all
+Test.delete_all
+User.delete_all
+Category.delete_all
 
 categories = Category.create!([{ name: 'IT' }, { name: 'Sport' }, { name: 'Art'}])
 
-users = User.create!([{ name: 'Veronika' }, { name: 'Victoria' }])
+users = User.create!([{ first_name: 'Vika', email: 'www@www.www', password: '111111', password_confirmation: '111111' },
+                      { first_name: 'Vera', email: 'vvv@vvv.vvv', password: '111111', password_confirmation: '111111' }])
+
+admin = Admin.create!({ first_name: 'Serg', last_name: 'Bury', email: 'zzz@zzz.zzz', password: '111111', password_confirmation: '111111' })
 
 tests = Test.create!([{ title: 'Painters', category: categories[2], level: 1, author: users[1] },
                       { title: 'Musicians', category: categories[2], author: users[0] }])
@@ -29,6 +33,6 @@ answers = Answer.create!([{ body: 'Michelangelo', question: questions[0] },
                           { body: 'Herman', question: questions[2] },
                           { body: 'Hercules', correct: true, question: questions[2] }])
 
-tests_users = TestsUser.create!([{ test: tests[0], user: users[0] },
+test_passages = TestPassage.create!([{ test: tests[0], user: users[0] },
                                  { test: tests[1], user: users[1] },
                                  { test: tests[0], user: users[1] }])
